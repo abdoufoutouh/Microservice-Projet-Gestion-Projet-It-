7bf3000e2a047c5fa77ec0958b4d95c59d1ae45f
Abderrahmane <foutouhabderrahman8@gmail.com>
2025-12-27 16:44:19 +0100

    Initial commit - project structure and microservices setup

---
+++ apigateway/src/test/java/com/projectit/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectit.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
