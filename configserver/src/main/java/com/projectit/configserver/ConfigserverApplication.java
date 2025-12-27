8a63af7f2eb183b567eb4ba9a179eb288a212491
Abderrahmane <foutouhabderrahman8@gmail.com>
2025-12-27 20:55:57 +0100

    Setup ConfigServer

--- configserver/src/main/java/com/projectit/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/projectit/configserver/ConfigserverApplication.java
@@ -1,7 +1,11 @@
 package com.projectit.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
 
 @SpringBootApplication
 public class ConfigserverApplication {
