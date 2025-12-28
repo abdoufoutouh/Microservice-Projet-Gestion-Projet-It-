1164cdc6a0e6f4baf71dcb417a18b67d03263df8
Abderrahmane <foutouhabderrahman8@gmail.com>
2025-12-28 02:10:34 +0100

    Implementation EurekaServer

--- eurekaserver/src/main/java/com/projectit/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/projectit/eurekaserver/EurekaserverApplication.java
@@ -1,8 +1,9 @@
 package com.projectit.eurekaserver;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaserverApplication {
 
